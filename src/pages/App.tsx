e8d8556a590f820b6da05d09c71165322940e8a6
wslyvh <wslyvh@users.noreply.github.com>
2020-05-13 09:36:53 +0200

    init

---
+++ src/pages/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Contraktor</h1>
+      </header>
+      <div>
+        <p>...</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
