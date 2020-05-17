2d82b0a8f919e8c37d4c0e431b18af5286112d42
wslyvh <wslyvh@users.noreply.github.com>
2020-05-17 13:15:56 +0200

    account control

--- src/components/AccountControl.tsx
+++ src/components/AccountControl.tsx
@@ -1,13 +1,34 @@
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { AccountInfo } from './AccountInfo';
 
-export const AccountControl = () => {
-    const userAddress = "0x039AE2898D257f4c9D7E381D478EE1ed042Fe2f4";
+declare let window: any
 
-    if (userAddress) { 
-        return <AccountInfo address={userAddress} />
+export const AccountControl = () => {
+    const [address, setAddress] = useState("");
+  
+    useEffect(() => {
+        if (window.ethereum?.selectedAddress) { 
+            const address = window.ethereum.selectedAddress;
+            
+            setAddress(address);
+        }    
+    }, []);
+      
+    async function connectWeb3() {
+        if (window.ethereum) { 
+            try {
+                const address = await window.ethereum.enable();
+                setAddress(address);
+            } catch (error) {
+                console.log(error);
+            }
+        } 
     }
 
-    return <button type="button" className="btn btn-outline-info btn-sm">Connect</button>
+    if (address) { 
+        return <AccountInfo address={address} /> 
+    }
+
+    return <button type="button" className="btn btn-outline-info btn-sm" onClick={connectWeb3}>Connect</button>
 }
