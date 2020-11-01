38d17f9dfa071c6f6dfbb6832a4685aa4995b34d
gasolin <gasolin@gmail.com>
2020-11-01 19:25:24 +0800

    fix tab to space

--- src/components/AccountControl.tsx
+++ src/components/AccountControl.tsx
@@ -1,27 +1,26 @@
-
 import React from 'react';
 import { AccountInfo } from './AccountInfo';
 import useWeb3Modal from "../hooks/useWeb3Modal";
 
 export const AccountControl = () => {
-	const {
-		account,
-		provider,
-		loadWeb3Modal,
-		logoutOfWeb3Modal,
-	} = useWeb3Modal();
-	const onClick = () => {
-		if (!provider) {
-			loadWeb3Modal();
-		} else {
-			logoutOfWeb3Modal();
-	  }
-	}
-	if (account) {
-			return <AccountInfo address={account} />
-	}
+  const {
+    account,
+    provider,
+    loadWeb3Modal,
+    logoutOfWeb3Modal,
+  } = useWeb3Modal();
+  const onClick = () => {
+    if (!provider) {
+      loadWeb3Modal();
+    } else {
+      logoutOfWeb3Modal();
+    }
+  }
+  if (account) {
+      return <AccountInfo address={account} />
+  }
 
-	return <button type="button" className="btn btn-outline-info btn-sm" onClick={onClick}>
-		{!provider ? "Connect Wallet" : "Disconnect Wallet"}
-	</button>
+  return <button type="button" className="btn btn-outline-info btn-sm" onClick={onClick}>
+    {!provider ? "Connect Wallet" : "Disconnect Wallet"}
+  </button>
 }
